ff299810768fbc3a39645aaad60a4c6069a94db8
Zajur Sajib <zakir@commandmedia.net>
2023-07-04 15:26:41 +0600

    netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
+import netlify from "@astrojs/netlify/functions";
 
+// https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  }),
+  adapter: netlify()
 });
