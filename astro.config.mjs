47fc94b6669195c36a13ba1b8d14a6c6c05598df
Zajur Sajib <zakir@commandmedia.net>
2023-07-04 15:59:18 +0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
-import netlify from "@astrojs/netlify/functions";
+import netlify from "@astrojs/netlify/edge-functions'";
 
 // https://astro.build/config
 export default defineConfig({
